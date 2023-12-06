b3cfce5a4e6e67e2c322e6afadafc6acb665e3dd
Tribeam <57662310+Tribeam@users.noreply.github.com>
2023-12-06 02:03:58 -0600

    Update colony.lua

--- Colony/colony.lua
+++ Colony/colony.lua
@@ -1,4 +1,18 @@
 
 
 
-print("hello world")
+
+
+
+
+
+
+local modem = peripheral.find("modem") or error("No modem attached", 0)
+
+local monitor = peripheral.find("monitor")
+if not monitor then error("Monitor not found.") end
+monitor.setTextScale(0.5)
+monitor.clear()
+monitor.setCursorPos(1, 1)
+monitor.setCursorBlink(false)
+print("Monitor initialized.")
